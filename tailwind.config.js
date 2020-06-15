0a46fe7574183d53511489a44fa86c935005c3d3
Avijit <avi_roy1988@hotmail.com>
2020-06-16 01:58:30 +0200

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { faRubleSign } = require("@fortawesome/free-solid-svg-icons");
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'black-t-50': 'rgba(0, 0, 0, .5)',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
